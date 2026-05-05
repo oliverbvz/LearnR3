39e2f37ef5f3e167595d101a5ea24854e4d29a1c
Oliver Bonde van Zwol <oliverbvz@bmb.sdu.dk>
2026-05-05 16:23:40 +0200

    used source and functions.R

---
+++ R/functions.R
@@ -0,0 +1,16 @@
+#' Read in nurses' stress data file
+#'
+#' @param file_path this is the oath to a data file
+#' @param max_rows this is the number of rows shown in the output. Default 100
+#'
+#'
+#' @returns outputs a dataframe/tibble
+read <- function(file_path, max_rows = 100) {
+  data <- file_path %>%
+    readr::read_csv( #to explicitly tell R studio to use read_csv from readr package
+      show_col_types = FALSE,
+      name_repair = snakecase::to_snake_case,
+      n_max = max_rows
+    )
+  return(data)
+}
